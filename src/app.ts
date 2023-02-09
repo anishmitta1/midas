66e6f1b824550c12919e6e45e13ce5fdba1d19ad
Anish Sai Mitta <anish.mitta@virgio.com>
2023-02-09 21:24:25 +0530

    create ts express template

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+import express from 'express';
+
+import type { Express } from 'express';
+
+const app: Express = express();
+const PORT_NUMBER = 3000;
+
+app.use((req, res, next) => {
+  const startTime = new Date().getTime();
+
+  res.on('finish', () => {
+    const timeElapsed = new Date().getTime() - startTime;
+    console.log(`Request took ${timeElapsed} ms`);
+  });
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello World!!');
+});
+
+app.listen(PORT_NUMBER, () => {
+  console.log(`Example app listening on port ${PORT_NUMBER}`);
+});
