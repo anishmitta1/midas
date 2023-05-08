d46976230d5dc4834f8692a70bfa17c0521459f2
anishmitta <anish.mitta@virgio.com>
2023-05-08 15:23:05 +0530

    refactoring

--- src/jobs/portfolio.ts
+++ src/jobs/portfolio.ts
@@ -1,51 +1,8 @@
-import axios from 'axios';
 import { logger } from '../instrumentation';
-import { STOP_LOSS_THRESHOLD } from '../constants/StopLoss';
 import { db } from '../firebase';
+import { portfolioService } from '../services';
 
 import type { IHolding } from '../types/holding';
-
-/**
- * Accepts holdings returned by the alpaca api and
- * sanitizes them as per our standard.
- * @param position
- * @returns
- */
-const toMidasHolding = (position: any): IHolding => {
-  return {
-    symbol: position.symbol,
-    stopLoss: {
-      buyPrice: Number(position.avg_entry_price),
-      lastTradedPrice: Number(position.market_value),
-      stopLoss: STOP_LOSS_THRESHOLD * position.avg_entry_price,
-      currentStopLoss: Math.max(
-        STOP_LOSS_THRESHOLD * position.market_value,
-        STOP_LOSS_THRESHOLD * position.avg_entry_price
-      ),
-    },
-  };
-};
-
-const getAlpacaHoldings = async (): Promise<IHolding[]> => {
-  const positionsUrl = `${process.env.ALPACA_API_BASE_URL}/v2/positions`;
-  const res = await axios({
-    method: 'get',
-    url: positionsUrl,
-    headers: {
-      'Apca-Api-Key-Id': process.env.ALPACA_API_KEY,
-      'Apca-Api-Secret-Key': process.env.ALPACA_API_SECRET,
-      'Content-Type': 'application/json',
-    },
-  });
-
-  const positions = res.data;
-
-  const alpacaHoldings = positions.map((position: any) =>
-    toMidasHolding(position)
-  );
-
-  return alpacaHoldings;
-};
 
 const updateHoldingsInFirestore = async (holdings: IHolding[]) => {
   const alpacaHoldingsRef = db.collection('holdings').doc('alpacaHoldings');
@@ -58,7 +15,7 @@ const updateHoldingsInFirestore = async (holdings: IHolding[]) => {
 
 const syncPortfolio = async () => {
   try {
-    const alpacaHoldings = await getAlpacaHoldings();
+    const alpacaHoldings = await portfolioService.getAllHoldings();
     await updateHoldingsInFirestore(alpacaHoldings);
   } catch (e) {
     logger.error(e);
